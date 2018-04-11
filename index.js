e5c7f8e5a3db3862560b600ddbf0143c48a80bb1
Arnaud <arnaud.bernard97@gmail.com>
2018-04-11 10:52:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import MyAnimeListHandler from "./handlers/myAnimeListHandler";
+
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+const token = process.env.TOKEN;
+
+client.on('ready', () => {
+    console.info('I am ready!');
+});
+
+client.on('message', async message => {
+    if (message.content.startsWith('/anime')) {
+        new MyAnimeListHandler(message);
+    }
+});
+
+client.login(token);
